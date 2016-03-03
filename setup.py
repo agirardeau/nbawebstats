24b37fcaa66c887de13b89686d8ed54e8813a6c0
agirardeaudale <agirardeaudale@users.noreply.github.com>
2016-03-02 16:02:30 -0800

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = []
 
 setup(
     name='pynba',
-    version='0.0.1',
+    version='0.0.2',
     description="Python interface to stats.nba.com HTTP API.",
     long_description=readme,
     author="Andrew Girardeau-Dale",
